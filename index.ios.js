d209155d789714fa7f5da25f9a9985b0dd95be90
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-08-22 14:57:03 -0400

    http get and transfer to state

--- index.ios.js
+++ index.ios.js
@@ -1,16 +1,19 @@
 //iOS!!
 // import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 
 //Imported components
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
-const App = function() {
+const App = () => {
   return(
-    //Make headerText as props
-    <Header headerText={'Albums'} />
+    <View>
+      <Header headerText={'Albums'} />
+      <AlbumList />
+    </View>
   );
 };
 
---
+++ src/components/AlbumList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+
+//Class based component
+class AlbumList extends Component {
+    //this is the state
+    state = { albums: [] };
+
+    //As soon as this component will be rendered to the screen this method will fire up
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => {
+                this.setState({ albums: response.data });
+            })
+    }
+
+    render() {
+        console.log(this.state);
+
+        return (
+            <View>
+                <Text> Hey Yoh! </Text>
+            </View>
+        );
+    }
+}
+
+export default AlbumList;
