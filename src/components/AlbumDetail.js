8f039788f53f1f1b09e545304b7dff24202afccd
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-08-23 11:29:09 -0400

    setting up containers

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+//Import Component
+import Card from './Card';
+import CardSection from './CardSection';
+
 const AlbumDetail = (props) => {
     //Props
     let album = {
-        title: props.album.title
+        title: props.album.title,
+        artist: props.album.artist,
+        url: props.album.url,
+        image: props.album.image,
+        thumbnail: props.album.thumbnail_image
     };
 
     //Render
     return (
-        <View>
-            <Text> {album.title} </Text>
-        </View>
+        <Card>
+            <CardSection>
+                <View>
+
+                </View>
+                <View style={styles.headerContentStyle}>
+                    <Text> {album.title} </Text>
+                    <Text> {album.artist} </Text>
+                </View>
+            </CardSection>
+        </Card>
     );
+};
+
+//Styling
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    }
 };
 
 export default AlbumDetail;
